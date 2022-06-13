387fe941820afb6c0e8d362c63edaff6d8810773
Lorna <liuchangyu1111@gmail.com>
2022-06-13 18:30:19 +0800

    Update train.py

--- train.py
+++ train.py
@@ -82,7 +82,7 @@ def main():
 
     for epoch in range(start_epoch, config.epochs):
         train(model, train_dataloader, criterion, optimizer, epoch, scaler, writer)
-        acc = validate(model, test_dataloader, epoch, writer, "valid")
+        acc = validate(model, test_dataloader, epoch, writer, "test")
         print("\n")
 
         # Automatically save the model with the highest index
